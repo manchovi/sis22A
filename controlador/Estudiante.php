a14ba421b0625d4a0e43ed405690753d326b91c6
manchovi <winmanuel07@gmail.com>
2018-04-10 15:41:25 -0600

    itca
    megatec

--- controlador/Estudiante.php
+++ controlador/Estudiante.php
@@ -1,64 +1,10 @@
 <?php
 public class Estudiante{
 
-public imprimir(){
-	echo "Manuel Gámez";
-}
-public imprimir(){
-        echo "Jimmy Mena";
+var Nombre;
+var Apellidos;
+var Direccion;
 
 
-public imprimir{
-	echo "saul elias garcia"
-}
-
-public imprimir{
-	echo "Hola soy walter y estoy aburrido"
-}
-
-
-public imprimir(){
-        echo "Erick ayala";
-}
-
-public imprimir(){
-        echo "Hola soy fran";
-}
-
-
-
-public imprimir(){
-        echo "Francisco Salvador :v";
-}
-
-public otromas(){
-echo "hola";
-}
-
-
-}
-public imprimir(){
-        echo "Jonathan Moises Calderon";
-}
-
-public imprimir(){
-	echo "estanley jose dominguez";
-}
-
-public imprimir(){
-	echo "Alejandro Gonzalez";
-}
-
-public imprimir(){
-	echo "Mario Rosa";
-}
-
-public mostrar(){
-	echo "Soy Francisco Chávez";
-}
-
-public mostrar(){
-	echo "Soy Victor González";
-}
 
 ?>
